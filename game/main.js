6216a4c3264ce0152d03f1e6052f4e66212731a4
Michael Ferguson <mike.ferguson2678@icloud.com>
2025-02-25 20:58:10 -0500

    Javascript
    Looks good

--- game/main.js
+++ game/main.js
@@ -6,17 +6,22 @@ gameGrid.append(computerChoiceDisplay, resultDisplay, userChoiceDisplay)
 
 const choices = ['rock', 'paper', 'scissors']
 let userChoice
+let computerChoice
+
 
 const handleclick = (e) => {
     userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
+    userChoiceDisplay.innerHTML = 'User choice:' + userChoice
     generateComputerChoice()
+    getResults()
 
 }
 
 const generateComputerChoice = () => {
 
     const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+    computerChoice = randomChoice
+    computerChoiceDisplay.innerHTML = 'Computer choice:' + randomChoice
 }
 for (let i = 0; i < choices.length; i++) {
     const button = document.createElement('button')
@@ -25,4 +30,31 @@ for (let i = 0; i < choices.length; i++) {
     button.addEventListener('click', handleclick)
     gameGrid.appendChild(button)
 }
+
+const getResults = () => {
+    switch (userChoice + computerChoice) {
+        case 'rockscissors':
+        case 'paperrock':
+        case 'scissorspaper':
+            resultDisplay.innerHTML = 'YOU WIN!'
+            break
+        case 'rockpaper':
+        case 'paperscissors':
+        case 'scissorsrock':
+            resultDisplay.innerHTML = 'YOU LOSE!'
+            break
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
+            resultDisplay.innerHTML = 'ITS A DRAW!'
+            break
+    }
+
+
+}
+
+
+
+
+
 
